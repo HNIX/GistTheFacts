7ff196f8f527a57dbb1d05f70bbefd1be15764fe
hnix <hnix.au@gmail.com>
2016-12-14 19:10:07 -0600

    Allow users to tag posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,23 @@
 class Post < ApplicationRecord
   validates :title, :body, presence: true
-
   belongs_to :user
-
+  has_many :taggings, dependent: :destroy
+  has_many :tags, through: :taggings
   delegate :username, to: :user
+
+  def self.tagged_with(name)
+    Tag.find_by!(name: name).posts
+  end
+
+  def all_tags=(names)
+    tags_array = names.split(",").map(&:strip)
+
+    self.tags = tags_array.map do |name|
+      Tag.where(name: name).first_or_create!
+    end
+  end
+
+  def all_tags
+    tags.map(&:name).join(", ")
+  end
 end
